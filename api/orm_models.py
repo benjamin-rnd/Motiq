60848112c8124093255adb682e57902e524d1bd8
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-06-13 23:57:16 +0200

    Add database conneciton and ORM models

---
+++ api/orm_models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Index
+from sqlalchemy.orm import Mapped, mapped_column
+from api.database import Base
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    event_name: Mapped[str] = mapped_column(nullable=False, index=True)
+    user_id: Mapped[str] = mapped_column(nullable=False)
+    session_id: Mapped[str] = mapped_column(nullable=False)
+    app_id: Mapped[str] = mapped_column(nullable=True)
+    timestamp: Mapped[str] = mapped_column(nullable=False)
+    properties: Mapped[str] = mapped_column(default="{}")
+
+    __table_args__ = (
+        Index("ix-event_name-user_id", "event_name", "user_id")
+    )
