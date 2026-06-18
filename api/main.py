b067b66215886cb0f20b158c12bdea31b93abe46
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-06-18 18:33:13 +0200

    Add GET endpoint for device breakdown

--- api/main.py
+++ api/main.py
@@ -12,6 +12,14 @@ app = FastAPI()
 def root(key: str = Depends(verify_api_key)):
     return {"message": "Hello! Look at http://localhost:8000/docs for documentation."}
 
+@app.get("/analytics/devices/breakdown")
+def get_device_breakdown(db: Session = Depends(get_db), key: str = Depends(verify_api_key)):
+    number_of_devices = crud.get_absolute_number_of_devices(db)
+    percentage_of_devices = crud.get_percentage_of_devices(db)
+
+    return {"number_of_devices": number_of_devices,
+            "percentage_of_devices": percentage_of_devices}
+
 # MARK: POST
 @app.post("/events/batch", status_code=status.HTTP_200_OK)
 def add_event_batch(batch: EventBatch, db: Session = Depends(get_db), key: str = Depends(verify_api_key)):
