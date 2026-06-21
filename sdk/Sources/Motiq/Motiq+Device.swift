f0ae5b6cf119da9d9b0392532e9cf0ac9c814542
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-06-21 11:59:11 +0200

    Add functions for collecting device data

--- sdk/Sources/Motiq/Motiq+Device.swift
+++ sdk/Sources/Motiq/Motiq+Device.swift
@@ -5,4 +5,64 @@
 //  Created by Benjamin Arndt on 21.06.26.
 //
 
-// collect device data: IDFV, 
+import Foundation
+import Network
+import UIKit
+
+extension Motiq {
+    
+    func getIDFV() async -> String {
+        return await UIDevice.current.identifierForVendor?.uuidString ?? "Unknown"
+    }
+    
+    func getAppVersion() -> String {
+        return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
+    }
+    
+    func getOSVersion() -> String {
+        let osVersion = ProcessInfo.processInfo.operatingSystemVersion
+        return "\(osVersion.majorVersion).\(osVersion.minorVersion).\(osVersion.patchVersion)"
+    }
+    
+    func getColorScheme() -> String {
+        return UITraitCollection.current.userInterfaceStyle == .dark ? "dark" : "light"
+    }
+    
+    func getOrientation() async -> String {
+        return await MainActor.run {
+            UIDevice.current.orientation.isLandscape ? "landscape" : "portrait"
+        }
+    }
+    
+    func getConnectivity() -> String {
+        let monitor = NWPathMonitor()
+        let path = monitor.currentPath
+        
+        if path.usesInterfaceType(.cellular) {
+            return "cellular"
+        } else if path.usesInterfaceType(.wifi) {
+            return "wifi"
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            return "ethernet"
+        } else {
+            return "Unknown"
+        }
+    }
+    
+    func getEnabledAccessibilityFeatures() async -> [String] {
+        let enabledFeatures = await MainActor.run {
+            var result: [String] = []
+            
+            if UIAccessibility.isBoldTextEnabled { result.append("bold_text") }
+            if UIAccessibility.isDarkerSystemColorsEnabled { result.append("darker_system_colors") }
+            if UIAccessibility.isReduceMotionEnabled { result.append("reduce_motion") }
+            if UIAccessibility.isReduceTransparencyEnabled { result.append("reduce_transparency") }
+            if UIAccessibility.isVoiceOverRunning { result.append("voice_over") }
+            
+            return result
+        }
+        
+        return enabledFeatures
+    }
+    
+}
