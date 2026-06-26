5135278cafbb962dc186004f1178479a6179bf68
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-06-27 01:58:24 +0200

    Remove comments

--- sdk/Sources/Motiq/Motiq+Session.swift
+++ sdk/Sources/Motiq/Motiq+Session.swift
@@ -5,15 +5,6 @@
 //  Created by Benjamin Arndt on 21.06.26.
 //
 
-/*
- MARK: ABLAUF:
- - bei App-Start wird configure() aufgerufen, also passiert Folgendes: SessionID generieren, observeAppLifecycle() registrieren, app_launched()
- - App ganz normal schließen bzw. in Hintergrund: app_closed
- - App aus Hintergrund wieder starten = "Mini-Neustart": neue SessionID & app_launched()
-   --> kein vollständiger Neustart, daher wird configure() auch nicht neu aufgerufen
- - Start nach Force-Close = kompletter Neustart, also wird configure() neu aufgerufen --> siehe oben
- */
-
 import Foundation
 import UIKit
 
