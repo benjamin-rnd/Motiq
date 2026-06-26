5274602126a4fab262b02f486eca5ad6ac93ad20
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-06-27 01:45:30 +0200

    Add logic for managing session

--- sdk/Sources/Motiq/Motiq+Session.swift
+++ sdk/Sources/Motiq/Motiq+Session.swift
@@ -5,4 +5,62 @@
 //  Created by Benjamin Arndt on 21.06.26.
 //
 
-// manage session IDs, recognize start and end of start via app lifecycle
+/*
+ MARK: ABLAUF:
+ - bei App-Start wird configure() aufgerufen, also passiert Folgendes: SessionID generieren, observeAppLifecycle() registrieren, app_launched()
+ - App ganz normal schließen bzw. in Hintergrund: app_closed
+ - App aus Hintergrund wieder starten = "Mini-Neustart": neue SessionID & app_launched()
+   --> kein vollständiger Neustart, daher wird configure() auch nicht neu aufgerufen
+ - Start nach Force-Close = kompletter Neustart, also wird configure() neu aufgerufen --> siehe oben
+ */
+
+import Foundation
+import UIKit
+
+extension Motiq {
+    
+    func generateNewSessionID() -> String {
+        UUID().uuidString
+    }
+    
+    func handleAppBackground() async {
+        await trackAppClose()
+    }
+    
+    func handleAppLaunchFromBackground() async {
+        sessionID = generateNewSessionID()
+        await trackAppLaunch()
+    }
+    
+    func handleAppTerminate() {
+        print("App terminated")
+        // TODO: Add offline queueing here --> will be added later
+    }
+    
+    func setupAppLifecycleObservers() {
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.didEnterBackgroundNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            Task { await self?.handleAppBackground() }
+        }
+        
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.willEnterForegroundNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            Task { await self?.handleAppLaunchFromBackground() }
+        }
+        
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.willTerminateNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            Task { await self?.handleAppTerminate() }
+        }
+    }
+    
+}
