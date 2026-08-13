9db4745f73fd6dfc3f0bf26e9504107d0564085e
benjamin-rnd <241163122+benjamin-rnd@users.noreply.github.com>
2026-08-13 21:49:43 +0200

    Remove root endpoint and OpenAPI documentation

--- api/main.py
+++ api/main.py
@@ -14,13 +14,9 @@ async def lifespan(app: FastAPI):
     yield
     # shutdown
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None, openapi_url=None)
 
 # MARK: GET
-@app.get("/")
-def root(key: str = Depends(verify_api_key)):
-    return {"message": "Hello! Look at http://localhost:8000/docs for documentation."}
-
 @app.get("/analytics/devices/breakdown", status_code=status.HTTP_200_OK)
 def get_device_breakdown(db: Session = Depends(get_db), key: str = Depends(verify_api_key)):
     number_of_devices = crud.get_absolute_number_of_devices(db)
